b9519b0c82f8ec7dc421dcf3840ddbca9a5073b0
Nyeso Zoltan <zoltan.nyeso@student.uni-miskolc.hu>
2025-05-06 18:30:53 +0200

    Kezdő felépítés

---
+++ src/app/components/quotation-details/quotation-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quotation-details',
+  imports: [],
+  templateUrl: './quotation-details.component.html',
+  styleUrl: './quotation-details.component.css'
+})
+export class QuotationDetailsComponent {
+
+}
---
+++ src/app/components/quotation-form/quotation-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quotation-form',
+  imports: [],
+  templateUrl: './quotation-form.component.html',
+  styleUrl: './quotation-form.component.css'
+})
+export class QuotationFormComponent {
+
+}
