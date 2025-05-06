400537ee7519fcf6bb02fd8734cf2cf6560523fa
Nyeso Zoltan <zoltan.nyeso@student.uni-miskolc.hu>
2025-05-06 22:06:26 +0200

    Dashboard finomítás+json betöltés

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -2,6 +2,8 @@ import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { Quote } from '../../_model/quote';
 import { FormsModule } from '@angular/forms';
+import { QuoteService } from '../../_services/quote.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -15,5 +17,10 @@ export class DashboardComponent {
   searchTerm: string = '';
   selectedCategory: string = '';
   selectedStatus: string = '';
+  quoteService = inject(QuoteService);
+  router = inject(Router);
 
+  ngOnInit(): void {
+    this.quotes = this.quoteService.loadQuotes();
+  }
 }
