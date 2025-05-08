3f169911ac82e9ee9e6d03ba158ae1af49b9932d
Nyeso Zoltan <zoltan.nyeso@student.uni-miskolc.hu>
2025-05-08 20:51:29 +0200

    Szűrők

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,14 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { Quote } from '../../_model/quote';
 import { FormsModule } from '@angular/forms';
-import { QuoteService } from '../../_services/quote.service';
 import { Router, RouterModule } from '@angular/router';
+import { Quote, Category, Mood, CATEGORY_OPTIONS, MOOD_OPTIONS } from '../../_model/quote';
+import { QuoteService } from '../../_services/quote.service';
 
 @Component({
   selector: 'app-dashboard',
+  standalone: true,
   imports: [CommonModule, FormsModule, RouterModule],
-  
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
@@ -16,8 +16,12 @@ export class DashboardComponent {
   quotes: Quote[] = [];
 
   searchTerm: string = '';
-  selectedCategory: string = '';
-  selectedStatus: string = '';
+  selectedCategory: Category | '' = '';
+  selectedMood: Mood | '' = '';
+
+  categoryOptions = CATEGORY_OPTIONS;
+  moodOptions = MOOD_OPTIONS;
+
   quoteService = inject(QuoteService);
   router = inject(Router);
 
@@ -25,15 +29,24 @@ export class DashboardComponent {
     this.quotes = this.quoteService.loadQuotes();
   }
 
-  onEdit(id: string) {
+  get filteredQuotes(): Quote[] {
+    return this.quotes.filter(quote =>
+      (quote.text.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
+       quote.author.toLowerCase().includes(this.searchTerm.toLowerCase())) &&
+      (this.selectedCategory ? quote.categories === this.selectedCategory : true) &&
+      (this.selectedMood ? quote.mood === this.selectedMood : true)
+    );
+  }
+
+  onEdit(id: string): void {
     this.router.navigate(['/edit-quote', id]);
   }
 
-  onView(id: string) {
+  onView(id: string): void {
     this.router.navigate(['/quote', id]);
   }
 
-  onDelete(id: string) {
+  onDelete(id: string): void {
     if (confirm('Biztosan törlöd ezt az idézetet?')) {
       this.quoteService.deleteQuote(id);
       this.quotes = this.quoteService.getAllQuotes();
